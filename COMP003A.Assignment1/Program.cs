5e89b52e9990fce5fb2340fe323b5e90a8679524
chave <chave@BRYANSWINDOW>
2024-01-21 20:31:54 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,121 @@
+﻿/* Author: Bryan Chavez
+ * Course: COMP-003A-L02
+ * Purpose: Code for assignment one
+ */
+
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am brown");
+            Console.WriteLine("I have brown eyes");
+            Console.WriteLine("I have dark brown hair");
+            Console.WriteLine("I have ten fingers");
+            Console.WriteLine("I have ten toes");
+            Console.WriteLine("I have curlyish hair");
+            Console.WriteLine("I can grow a beard");
+            Console.WriteLine("I currently have a goatee");
+            Console.WriteLine("I have very low patience");
+            Console.WriteLine("I hate useless assignments");
+            Console.WriteLine("I played football");
+            Console.WriteLine("I was top hundred in Fresno County");
+            Console.WriteLine("I live in Riverdale ");
+            Console.WriteLine("I was Born in Oxnard");
+            Console.WriteLine("I love listening to music");
+            Console.WriteLine("I grew up poor");
+            Console.WriteLine("I am technecally broke still");
+            Console.WriteLine("I loved weight lifting");
+            Console.WriteLine("I injured myself weight lifting");
+            Console.WriteLine("I lost 60 pluis pounds");
+            Console.WriteLine("I am still currently hurt from my back");
+            Console.WriteLine("I am still hurt from my shoulders");
+            Console.WriteLine("I am constantly in pain");
+            Console.WriteLine("I am always angry");
+            Console.WriteLine("I used to play the piano a little");
+            Console.WriteLine("I like android");
+            Console.WriteLine("I dislike apple products");
+            Console.WriteLine("I'm already sick of writong this");
+            Console.WriteLine("I am turning 20 this year");
+            Console.WriteLine("I love reading books");
+            Console.Write("My favorite genere of book is romance");
+            Console.WriteLine("My favorite author is Collen Hover");
+            Console.WriteLine("Yes I know its basic");
+            Console.WriteLine("I like black");
+            Console.WriteLine("I am currently listening to heacy metal music");
+            Console.WriteLine("My favorite band is Dance Gavin Dance");
+            Console.WriteLine("My favorite song from them is Count Bassy");
+            Console.WriteLine("Im currentlky listening to Emapthy,A Gift");
+            Console.WriteLine("I am also really liking the band Nothing but Thieves");
+            Console.WriteLine("I was a offensive gaurd");
+            Console.WriteLine("I was also a defensive tackle");
+            Console.WriteLine("My Favorite NFL team is the LA Rams");
+            Console.WriteLine("My favorite player is Aaron Donald");
+            Console.WriteLine("I hate the 49r's");
+            Console.WriteLine("I am single");
+            Console.WriteLine("I have no idea what I'm going to do");
+            Console.WriteLine("My");
+            Console.WriteLine("Back");
+            Console.WriteLine("Is");
+            Console.WriteLine("Killing");
+            Console.WriteLine("ME");
+            Console.WriteLine("Can't belive the Packers lost with a pick");
+            Console.WriteLine("I have 4 siblings");
+            Console.WriteLine("They are all boys");
+            Console.WriteLine("Your blood fromnothging but theives is my favorite song right now");
+            Console.WriteLine("I love hanging out with my friends");
+            Console.WriteLine("I love distractions");
+            Console.WriteLine("I hate having to much time to think");
+            Console.WriteLine("I'm not the biggest fan of school");
+            Console.WriteLine("I have made Dean's List all 3 semsters");
+            Console.WriteLine("I");
+            Console.WriteLine("Hope");
+            Console.WriteLine("I");
+            Console.WriteLine("Decently");
+            Console.WriteLine("Like");
+            Console.WriteLine("This");
+            Console.WriteLine("Class");
+            Console.WriteLine("and");
+            Console.WriteLine("Coding");
+            Console.WriteLine("I drive a Jeep");
+            Console.WriteLine("I really want a motorcycle");
+            Console.WriteLine("I am not afraid to die");
+            Console.WriteLine("I am afraid of being killed");
+            Console.WriteLine("I currently have a handheald retro gaming device");
+            Console.WriteLine("Favorite game on it is Doom 64");
+            Console.WriteLine("I keep getting screwed over by my clinic");
+            Console.WriteLine("I'm trying to set up physical thearapy");
+            Console.WriteLine("I used to be a big extrovert");
+            Console.WriteLine("I hate going out to places I don't like with alot of people");
+            Console.WriteLine("I dislike talking to people I don't know");
+            Console.WriteLine("I really love food");
+            Console.WriteLine("I am five foot eleven inches");
+            Console.WriteLine("My bestfriend is Hector");
+            Console.WriteLine("My favorite parts of football was winning and hitting people");
+            Console.WriteLine("I once lived in a household of over 12 people");
+            Console.WriteLine("I'm going to transfer to Fresno State after this semester");
+            Console.WriteLine("I plan onmajoring in Business Managment and minor in Computer Science");
+            Console.WriteLine("I want to start weight lifting again");
+            Console.WriteLine("I want to drop to 185");
+            Console.WriteLine("This");
+            Console.WriteLine("Is");
+            Console.WriteLine("Really");
+            Console.WriteLine("Getting");
+            Console.WriteLine("On");
+            Console.WriteLine("My");
+            Console.WriteLine("Nerves");
+            Console.WriteLine("a");
+            Console.WriteLine("tiny");
+            Console.WriteLine("BIT");
+
+              
+
+           
+
+
+
+        }
+    }
+}
